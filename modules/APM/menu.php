6d17dcd578325a4918e48bb28c91e5ec3510af05
Basoro <dentix.id@gmail.com>
2020-03-01 10:55:38 +0800

    Update

--- modules/APM/menu.php
+++
@@ -1,6 +0,0 @@
-<li class="<?php if ($params['module']=="APM") {echo "active"; } ?>">
-    <a href="<?php echo URL; ?>/index.php?module=APM&page=index">
-        <i class="material-icons">aspect_ratio</i>
-        <span>APM</span>
-    </a>
-</li>
