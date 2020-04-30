73839f8731019352f2267e1a92e327d30f19bb22
Basoro <dentix.id@gmail.com>
2020-04-30 14:16:41 +0800

    Update

--- plugins/master/Admin.php
+++ plugins/master/Admin.php
@@ -152,7 +152,7 @@ class Admin extends AdminModule
       $pdf->Text(10, 40, 'DATA PEGAWAI');
       $pdf->Ln(34);
       $pdf->SetFont('Arial', '', 10);
-      $pdf->SetWidths(array(20,80,25,25,40));
+      $pdf->SetWidths(array(50,70,25,25,20));
       $pdf->Row(array('Kode Pegawai','Nama Pegawai','Tempat Lahir', 'Tanggal Lahir', 'Status'));
       foreach ($pasien as $hasil) {
         $pdf->Row(array($hasil['nik'],$hasil['nama'],$hasil['tmp_lahir'],$hasil['tgl_lahir'],$hasil['stts_aktif']));
