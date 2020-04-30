d1613cf6ad837e898b7ae0bc9f59ac684eedb3c5
Basoro <dentix.id@gmail.com>
2020-04-30 20:37:12 +0800

    Update

---
+++ plugins/kepegawaian/Admin.php
@@ -0,0 +1,257 @@
+<?php
+
+namespace Plugins\Kepegawaian;
+
+use Systems\AdminModule;
+use Systems\Lib\Fpdf\PDF_MC_Table;
+
+class Admin extends AdminModule
+{
+    public function navigation()
+    {
+        return [
+            'Data Pegawai' => 'manage',
+            'Tambah Baru' => 'add',
+            'Master' => 'master',
+        ];
+    }
+
+    /* Master Pegawai Section */
+    public function getManage($page = 1)
+    {
+        $perpage = '10';
+
+        $phrase = '';
+        if(isset($_GET['s']))
+          $phrase = $_GET['s'];
+
+        // pagination
+        $totalRecords = $this->db()->pdo()->prepare("SELECT * FROM pegawai WHERE (nik LIKE ? OR nama LIKE ?)");
+        $totalRecords->execute(['%'.$phrase.'%', '%'.$phrase.'%']);
+        $totalRecords = $totalRecords->fetchAll();
+
+        $pagination = new \Systems\Lib\Pagination($page, count($totalRecords), 10, url([ADMIN, 'kepegawaian', 'manage', '%d']));
+        $this->assign['pagination'] = $pagination->nav('pagination','5');
+        $this->assign['totalRecords'] = $totalRecords;
+
+        // list
+        $offset = $pagination->offset();
+        $query = $this->db()->pdo()->prepare("SELECT * FROM pegawai WHERE (nik LIKE ? OR nama LIKE ?) LIMIT $perpage OFFSET $offset");
+        $query->execute(['%'.$phrase.'%', '%'.$phrase.'%']);
+        $rows = $query->fetchAll();
+
+        $this->assign['list'] = [];
+        if (count($rows)) {
+            foreach ($rows as $row) {
+                $row = htmlspecialchars_array($row);
+                $row['editURL'] = url([ADMIN, 'kepegawaian', 'edit', $row['id']]);
+                $row['viewURL'] = url([ADMIN, 'kepegawaian', 'view', $row['id']]);
+                $this->assign['list'][] = $row;
+            }
+        }
+
+        $this->assign['getStatus'] = isset($_GET['status']);
+        $this->assign['printURL'] = url([ADMIN, 'kepegawaian', 'print']);
+
+        return $this->draw('manage.html', ['pegawai' => $this->assign]);
+
+    }
+
+    public function getAdd()
+    {
+        $this->_addHeaderFiles();
+        if (!empty($redirectData = getRedirectData())) {
+            $this->assign['form'] = filter_var_array($redirectData, FILTER_SANITIZE_STRING);
+        } else {
+            $this->assign['form'] = [
+              'nik' => '',
+              'nama' => '',
+              'jk' => '',
+              'jbtn' => '',
+              'jnj_jabatan' => '',
+              'kode_kelompok' => '',
+              'kode_resiko' => '',
+              'kode_emergency' => '',
+              'departemen' => '',
+              'bidang' => '',
+              'stts_wp' => '',
+              'stts_kerja' => '',
+              'npwp' => '',
+              'pendidikan' => '',
+              'gapok' => '',
+              'tmp_lahir' => '',
+              'tgl_lahir' => '',
+              'alamat' => '',
+              'kota' => '',
+              'mulai_kerja' => '',
+              'ms_kerja' => '',
+              'indexins' => '',
+              'bpd' => '',
+              'rekening' => '',
+              'stts_aktif' => '',
+              'wajibmasuk' => '',
+              'pengurang' => '',
+              'indek' => '',
+              'mulai_kontrak' => '',
+              'cuti_diambil' => '',
+              'dankes' => '',
+              'photo' => '',
+              'no_ktp' => ''
+            ];
+        }
+
+        $this->assign['title'] = 'Tambah Pegawai';
+        $this->assign['jk'] = $this->_addEnum('pegawai', 'jk');
+        $this->assign['ms_kerja'] = $this->_addEnum('pegawai', 'ms_kerja');
+        $this->assign['stts_aktif'] = $this->_addEnum('pegawai', 'stts_aktif');
+        $this->assign['jnj_jabatan'] = $this->db('jnj_jabatan')->toArray();
+        $this->assign['kelompok_jabatan'] = $this->db('kelompok_jabatan')->toArray();
+        $this->assign['resiko_kerja'] = $this->db('resiko_kerja')->toArray();
+        $this->assign['departemen'] = $this->db('departemen')->toArray();
+        $this->assign['bidang'] = $this->db('bidang')->toArray();
+        $this->assign['stts_wp'] = $this->db('stts_wp')->toArray();
+        $this->assign['stts_kerja'] = $this->db('stts_kerja')->toArray();
+        $this->assign['pendidikan'] = $this->db('pendidikan')->toArray();
+        $this->assign['bank'] = $this->db('bank')->toArray();
+        $this->assign['emergency_index'] = $this->db('emergency_index')->toArray();
+
+        $this->assign['fotoURL'] = url(MODULES.'/users/img/default.png');
+
+        return $this->draw('form.html', ['pegawai' => $this->assign]);
+    }
+
+    public function getEdit($id)
+    {
+        $this->_addHeaderFiles();
+        $user = $this->db('pegawai')->oneArray($id);
+        if (!empty($user)) {
+            $this->assign['form'] = $user;
+            $this->assign['title'] = 'Edit Pegawai';
+
+            $this->assign['jk'] = $this->_addEnum('pegawai', 'jk');
+            $this->assign['ms_kerja'] = $this->_addEnum('pegawai', 'ms_kerja');
+            $this->assign['stts_aktif'] = $this->_addEnum('pegawai', 'stts_aktif');
+            $this->assign['jnj_jabatan'] = $this->db('jnj_jabatan')->toArray();
+            $this->assign['kelompok_jabatan'] = $this->db('kelompok_jabatan')->toArray();
+            $this->assign['resiko_kerja'] = $this->db('resiko_kerja')->toArray();
+            $this->assign['departemen'] = $this->db('departemen')->toArray();
+            $this->assign['bidang'] = $this->db('bidang')->toArray();
+            $this->assign['stts_wp'] = $this->db('stts_wp')->toArray();
+            $this->assign['stts_kerja'] = $this->db('stts_kerja')->toArray();
+            $this->assign['pendidikan'] = $this->db('pendidikan')->toArray();
+            $this->assign['bank'] = $this->db('bank')->toArray();
+            $this->assign['emergency_index'] = $this->db('emergency_index')->toArray();
+
+            $this->assign['fotoURL'] = url(MODULES.'/users/img/default.png');
+            return $this->draw('form.html', ['pegawai' => $this->assign]);
+        } else {
+            redirect(url([ADMIN, 'kepegawaian', 'manage']));
+        }
+    }
+
+    public function postSave($id = null)
+    {
+        $errors = 0;
+
+        if (!$id) {
+            $location = url([ADMIN, 'kepegawaian', 'add']);
+        } else {
+            $location = url([ADMIN, 'kepegawaian', 'edit', $id]);
+        }
+
+        if (checkEmptyFields(['nik', 'nama'], $_POST)) {
+            $this->notify('failure', 'Isian kosong');
+            redirect($location, $_POST);
+        }
+
+        if (!$errors) {
+            unset($_POST['save']);
+
+            if (!$id) {    // new
+                $query = $this->db('pegawai')->save($_POST);
+            } else {        // edit
+                $query = $this->db('pegawai')->where('id', $id)->save($_POST);
+            }
+
+            if ($query) {
+                $this->notify('success', 'Simpan sukes');
+            } else {
+                $this->notify('failure', 'Simpan gagal');
+            }
+
+            redirect($location);
+        }
+
+        redirect($location, $_POST);
+    }
+
+    public function getPrint()
+    {
+      $pasien = $this->db('pegawai')->toArray();
+      $logo = 'data:image/png;base64,' . base64_encode($this->core->getSettings('logo'));
+
+      $pdf = new PDF_MC_Table();
+      $pdf->AddPage();
+      $pdf->SetAutoPageBreak(true, 10);
+      $pdf->SetTopMargin(10);
+      $pdf->SetLeftMargin(10);
+      $pdf->SetRightMargin(10);
+
+      $pdf->Image($logo, 10, 8, '18', '18', 'png');
+      $pdf->SetFont('Arial', '', 24);
+      $pdf->Text(30, 16, $this->core->getSettings('nama_instansi'));
+      $pdf->SetFont('Arial', '', 10);
+      $pdf->Text(30, 21, $this->core->getSettings('alamat_instansi').' - '.$this->core->getSettings('kabupaten'));
+      $pdf->Text(30, 25, $this->core->getSettings('kontak').' - '.$this->core->getSettings('email'));
+      $pdf->Line(10, 30, 200, 30);
+      $pdf->Line(10, 31, 200, 31);
+      $pdf->Text(10, 40, 'DATA PEGAWAI');
+      $pdf->Ln(34);
+      $pdf->SetFont('Arial', '', 10);
+      $pdf->SetWidths(array(50,70,25,25,20));
+      $pdf->Row(array('Kode Pegawai','Nama Pegawai','Tempat Lahir', 'Tanggal Lahir', 'Status'));
+      foreach ($pasien as $hasil) {
+        $pdf->Row(array($hasil['nik'],$hasil['nama'],$hasil['tmp_lahir'],$hasil['tgl_lahir'],$hasil['stts_aktif']));
+      }
+      $pdf->Output('laporan_pegawai_'.date('Y-m-d').'.pdf','I');
+
+    }
+    /* End Master Poliklinik Section */
+
+
+    public function getCSS()
+    {
+        header('Content-type: text/css');
+        echo $this->draw(MODULES.'/kepegawaian/css/admin/kepegawaian.css');
+        exit();
+    }
+
+    public function getJavascript()
+    {
+        header('Content-type: text/javascript');
+        echo $this->draw(MODULES.'/kepegawaian/js/admin/kepegawaian.js');
+        exit();
+    }
+
+    private function _addHeaderFiles()
+    {
+        // CSS
+        $this->core->addCSS(url('assets/css/jquery-ui.css'));
+
+        // JS
+        $this->core->addJS(url('assets/jscripts/jquery-ui.js'), 'footer');
+
+        // MODULE SCRIPTS
+        $this->core->addCSS(url([ADMIN, 'kepegawaian', 'css']));
+        $this->core->addJS(url([ADMIN, 'kepegawaian', 'javascript']), 'footer');
+    }
+
+    private function _addEnum($table_name, $column_name) {
+      $result = $this->db()->pdo()->prepare("SHOW COLUMNS FROM $table_name LIKE '$column_name'");
+      $result->execute();
+      $result = $result->fetch();
+      $result = explode("','",preg_replace("/(enum|set)\('(.+?)'\)/","\\2", $result[1]));
+      return $result;
+    }
+
+}
---
+++ plugins/kepegawaian/Info.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'name'          =>  'Kepegawaian',
+    'description'   =>  'Pengelolaan data kepegawaian Khanza LITE.',
+    'author'        =>  'Basoro',
+    'version'       =>  '1.0',
+    'compatibility' =>  '3.*',
+    'icon'          =>  'group',
+];
