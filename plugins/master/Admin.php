aa18d2a6c01fce1b3f88b343e4c10d46db1ac00f
Basoro <dentix.id@gmail.com>
2020-04-30 15:01:03 +0800

    Update

--- plugins/master/Admin.php
+++ plugins/master/Admin.php
@@ -68,13 +68,42 @@ class Admin extends AdminModule
             $this->assign['form'] = [
               'nik' => '',
               'nama' => '',
+              'jk' => '',
+              'jbtn' => '',
+              'jnj_jabatan' => '',
+              'kode_kelompok' => '',
+              'kode_resiko' => '',
+              'kode_emergency' => '',
+              'departemen' => '',
+              'bidang' => '',
+              'stts_wp' => '',
+              'stts_kerja' => '',
+              'npwp' => '',
+              'pendidikan' => '',
+              'gapok' => '',
               'tmp_lahir' => '',
               'tgl_lahir' => '',
-              'stts_aktif' => ''
+              'alamat' => '',
+              'kota' => '',
+              'mulai_kerja' => '',
+              'ms_kerja' => '',
+              'indexins' => '',
+              'bpd' => '',
+              'rekening' => '',
+              'stts_aktif' => '',
+              'wajibmasuk' => '',
+              'pengurang' => '',
+              'indek' => '',
+              'mulai_kontrak' => '',
+              'cuti_diambil' => '',
+              'dankes' => '',
+              'photo' => '',
+              'no_ktp' => ''
             ];
         }
 
         $this->assign['title'] = 'Tambah Pegawai';
+        $this->assign['jk'] = $this->_addEnum('pegawai', 'jk');
 
         return $this->draw('pegawai.form.html', ['pegawai' => $this->assign]);
     }
