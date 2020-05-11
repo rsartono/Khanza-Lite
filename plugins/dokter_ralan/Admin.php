f638204a8271ae074569044698b5fabf766cac7f
Basoro <dentix.id@gmail.com>
2020-05-11 08:31:46 +0800

    Update Admin.php

--- plugins/dokter_ralan/Admin.php
+++ plugins/dokter_ralan/Admin.php
@@ -29,36 +29,40 @@ class Admin extends AdminModule
         $phrase = $_GET['s'];
 
       // pagination
-      $totalRecords = $this->db('reg_periksa')
-        ->like('reg_periksa.no_rkm_medis', '%'.$phrase.'%')
-        ->like('reg_periksa.no_rawat', '%'.$phrase.'%')
-        ->where('reg_periksa.status_lanjut', 'Ralan')
-        ->where('reg_periksa.tgl_registrasi', '>=', $start_date)
-        ->where('reg_periksa.tgl_registrasi', '<=', $end_date)
-        ->where('reg_periksa.kd_dokter', $_SESSION['opensimrs_username'])
-        ->join('pasien', 'pasien.no_rkm_medis = reg_periksa.no_rkm_medis')
-        ->like('pasien.nm_pasien', '%'.$phrase.'%')
-        ->toArray();
+      $totalRecords = $this->db()->pdo()
+        ->prepare("SELECT reg_periksa.*
+          FROM reg_periksa, pasien
+          WHERE reg_periksa.no_rkm_medis = pasien.no_rkm_medis
+          AND (reg_periksa.no_rkm_medis LIKE ? OR reg_periksa.no_rawat LIKE ? OR pasien.nm_pasien LIKE ?)
+          AND reg_periksa.status_lanjut = 'Ralan'
+          AND reg_periksa.tgl_registrasi BETWEEN '$start_date' AND '$end_date'");
+      $totalRecords->execute(['%'.$phrase.'%', '%'.$phrase.'%', '%'.$phrase.'%']);
+      $totalRecords = $totalRecords->fetchAll();
+
       $pagination = new \Systems\Lib\Pagination($page, count($totalRecords), $perpage, url([ADMIN, 'dokter_ralan', 'manage', '%d?s='.$phrase.'&start_date='.$start_date.'&end_date='.$end_date]));
       $this->assign['pagination'] = $pagination->nav('pagination','5');
       $this->assign['totalRecords'] = $totalRecords;
 
       $offset = $pagination->offset();
-      $rows = $this->db('reg_periksa')
-        ->like('reg_periksa.no_rkm_medis', '%'.$phrase.'%')
-        ->like('reg_periksa.no_rawat', '%'.$phrase.'%')
-        ->where('reg_periksa.status_lanjut', 'Ralan')
-        ->where('reg_periksa.tgl_registrasi', '>=', $start_date)
-        ->where('reg_periksa.tgl_registrasi', '<=', $end_date)
-        ->where('reg_periksa.kd_dokter', $_SESSION['opensimrs_username'])
-        ->join('pasien', 'pasien.no_rkm_medis = reg_periksa.no_rkm_medis')
-        ->like('pasien.nm_pasien', '%'.$phrase.'%')
-        ->join('poliklinik', 'poliklinik.kd_poli = reg_periksa.kd_poli')
-        ->join('dokter', 'dokter.kd_dokter = reg_periksa.kd_dokter')
-        ->join('penjab', 'penjab.kd_pj = reg_periksa.kd_pj')
-        ->offset($offset)
-        ->limit($perpage)
-        ->toArray();
+      $query = $this->db()->pdo()
+        ->prepare("SELECT reg_periksa.*,
+            pasien.nm_pasien,
+            pasien.alamat,
+            dokter.nm_dokter,
+            poliklinik.nm_poli,
+            penjab.png_jawab
+          FROM reg_periksa, pasien, dokter, poliklinik, penjab
+          WHERE reg_periksa.no_rkm_medis = pasien.no_rkm_medis
+          AND reg_periksa.status_lanjut = 'Ralan'
+          AND reg_periksa.tgl_registrasi BETWEEN '$start_date' AND '$end_date'
+          AND (reg_periksa.no_rkm_medis LIKE ? OR reg_periksa.no_rawat LIKE ? OR pasien.nm_pasien LIKE ?)
+          AND reg_periksa.kd_dokter = dokter.kd_dokter
+          AND reg_periksa.kd_poli = poliklinik.kd_poli
+          AND reg_periksa.kd_pj = penjab.kd_pj
+          LIMIT $perpage
+          OFFSET $offset");
+      $query->execute(['%'.$phrase.'%', '%'.$phrase.'%', '%'.$phrase.'%']);
+      $rows = $query->fetchAll();
 
       $this->assign['list'] = [];
       if (count($rows)) {
