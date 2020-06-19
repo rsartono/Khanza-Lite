4ab7982b15f058aa614ee6da1f19267006b639f2
Basoro <dentix.id@gmail.com>
2020-06-20 04:37:20 +0800

    Update

--- plugins/ralan/Info.php
+++ plugins/ralan/Info.php
@@ -3,7 +3,7 @@ return [
     'name'          =>  'Rawat Jalan',
     'description'   =>  'Pengelolaan data pasien rawat jalan.',
     'author'        =>  'Basoro',
-    'version'       =>  '1.1',
+    'version'       =>  '1.2',
     'compatibility' =>  '3.*',
     'icon'          =>  'wheelchair',
 ];
